ea3f4443f68d1f2e81cbfe7c943ad54ce4840c1b
gediont <teshomegedion@gmail.com>
2019-11-17 10:41:21 +0300

    added config.json for jwt-secret and connection string and db connection

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const config    = require('./config.json');
+const mongoose  = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+// Get current connected Database
+var db = mongoose.connection;
+
+// Notify on error or success
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => console.log("Connected to the Database!"));
+
+module.exports = {
+    User:    require('../src/users/userModel'), 
+    Profile: require('../src/profile/profileModel'),
+    Campus:  require('../src/campus/campusModel')
+};
