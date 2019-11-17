734f70719e06798f6208bd401f1562f74dc3ff61
gediont <teshomegedion@gmail.com>
2019-11-17 15:22:40 +0300

    added a user dal

---
+++ src/users/userDal.js
@@ -0,0 +1,25 @@
+const User  =   require('./userModel');
+
+module.exports = {
+    create,
+    update, 
+    findOne,
+    findAll
+}
+
+async function create(data) {
+    return await User(data).save();
+}
+
+async function update(user, data) {
+    Object.assign(user, data);
+    return await user.save();
+}
+
+async function findOne(query) {
+    return await User.findOne(query).populate('profile').exec();
+}
+
+async function findAll(query) {
+    return await User.find(query).exec();
+}
