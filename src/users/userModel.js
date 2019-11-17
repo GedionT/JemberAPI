a681a00be9183516d60ab3cd2425546b6e1d5b22
gediont <teshomegedion@gmail.com>
2019-11-17 15:22:27 +0300

    added profile filed in user model

--- src/users/userModel.js
+++ src/users/userModel.js
@@ -4,6 +4,7 @@ var Schema       = mongoose.Schema;
 const userSchema = new Schema({
     username : {type: String, lowercase: true, required: true, match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
     phone    : {type: String, required: true},
+    profile  : {type: Schema.Types.ObjectId, ref: 'Profile'},
     hash     : {type: String, required: false}
 }, {
   timestamps : {createdAt: 'created_at', updatedAt: 'modified_at' }
