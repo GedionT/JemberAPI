a62ecb7096ddfb26b967514eaa5fab5873b84a6e
gediont <teshomegedion@gmail.com>
2019-11-17 10:37:23 +0300

    added working auth middleware code

---
+++ services/authMiddleware.js
@@ -0,0 +1,18 @@
+let jwt         = require('jsonwebtoken');
+const config    = require('../lib/config.json');
+
+
+async function auth (req, res, next) {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, config.secret);
+    
+        req.userData = decoded;
+       
+        next();
+    }  catch (error){
+        res.status(401).send({error: 'Not authorized to access this resource'});
+    }
+}
+
+module.exports = auth;
