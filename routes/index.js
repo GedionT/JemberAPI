331038120aa6a5c9f990aabd328f4bf21be39c14
gediont <teshomegedion@gmail.com>
2019-11-17 12:48:33 +0300

    minor fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var app           = require('../app');
+var router        = require('express').Router();
 
-var userRouter    = require('./src/users/userRoutes');
-var profileRouter = require('./src/profile/profileRoutes');
-var campusRouter  = require('./src/campus/campusRoutes');
+var userRouter    = require('../src/users/userRoutes');
+var profileRouter = require('../src/profile/profileRoutes');
+var campusRouter  = require('../src/campus/campusRoutes');
+var fileRouter    = require('../src/files/fileRoutes');
 
-app.use('/users', userRouter);
-app.use('/campus', campusRouter);
-app.use('/profile', profileRouter);
+router.use('/users', userRouter);
+router.use('/profile', profileRouter);
+router.use('/campus', campusRouter);
+router.use('/files', fileRouter);
+
+module.exports = router;
