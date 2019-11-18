d8e9bd04ee62ce25edb0ef33fe9f72d8bbccb8a0
gediont <teshomegedion@gmail.com>
2019-11-18 14:13:20 +0300

    renamed the application name

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes setup
-app.use('/chora', require('./routes'));
+app.use('/jember', require('./routes'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
