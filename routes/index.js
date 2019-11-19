0e6f920c92c5e7d37243aa5074c82a740e43dac9
gediont <teshomegedion@gmail.com>
2019-11-19 16:33:41 +0300

    commented unfinished features

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var router        = require('express').Router();
 
 router.use('/users', require('../src/users/userRoutes'));
-router.use('/profile', require('../src/profile/profileRoutes'));
-router.use('/campus', require('../src/campus/campusRoutes'));
-router.use('/files', require('../src/files/fileRoutes'));
+// router.use('/profile', require('../src/profile/profileRoutes'));
+// router.use('/campus', require('../src/campus/campusRoutes'));
+// router.use('/files', require('../src/files/fileRoutes'));
 
 module.exports = router;
