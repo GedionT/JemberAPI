c46d268534e989fbf07967aca9e109ee441786be
gediont <teshomegedion@gmail.com>
2019-11-20 12:28:02 +0300

    added error handler middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError   = require('http-errors');
 var express       = require('express');
 var path          = require('path');
 var cookieParser  = require('cookie-parser');
+var errorHandler  = require('./services/errorHandler');
 var logger        = require('morgan');
 
 var app = express();
@@ -26,14 +27,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(app.use(errorHandler));
 
 module.exports = app;
