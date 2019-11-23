74650db7b61220325adf638e7dfb811e6be7e9e7
gediont <teshomegedion@gmail.com>
2019-11-23 14:04:11 +0300

    minor addition on file uploader.js

--- services/uploader.js
+++ services/uploader.js
@@ -1,7 +1,15 @@
 var cloudinary   = require('cloudinary').v2;
 
+module.exports = {
+    imgUpload,
+    fileUpload,
+    videoUpload,
+}
+
 cloudinary.config({
     cloud_name: 'jember',
     api_key: '244988797972126',
     api_secret: 'MxgjYxIm74MpjwVCeLByE8k6vFk'
 });
+
+
