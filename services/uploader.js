127defaac895bd067976585af1d7f04d854f73ed
gediont <teshomegedion@gmail.com>
2019-11-23 13:07:20 +0300

    added configurations for upload to cloudinary

--- services/uploader.js
+++ services/uploader.js
@@ -1,7 +1,7 @@
 var cloudinary   = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'jemberBucket',
-    api_key: '',
-    api_secret: ''
+    cloud_name: 'jember',
+    api_key: '244988797972126',
+    api_secret: 'MxgjYxIm74MpjwVCeLByE8k6vFk'
 });
