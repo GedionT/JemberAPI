cc800da0c57e9b9ce3a6567610be3e32c21ed63d
gediont <teshomegedion@gmail.com>
2019-11-24 13:39:43 +0300

    working on profile controller i.e. update

--- src/profile/profileController.js
+++ src/profile/profileController.js
@@ -1,5 +1,6 @@
 const config      = require('../../config/config.json');
 const profileDal  = require('./profileDal');
+const userDal     = require('../users/userDal');
 const validator   = require('../../services/validator');
 const upload      = require('../../services/fileUpload');
 
@@ -12,8 +13,21 @@ module.exports = {
     getAll
 }
 
-async function update (req, res) {
+async function update (req, res, next) {
+    let id = req.params.id;
+    let body = req.body;
 
+    await userDal.findOne({id})
+        .then(user => {
+            if(!user) throw 'User not found';
+            if(user.username !== body.username && await userDal.findOne({ 'username': body.username })){
+                throw 'User name is already taken';
+            }
+            
+            var updated = await profileDal.update(user, body);
+            res.status(200).json({ message: 'Profile updated', updated});
+        })
+        .catch(err => next(err));
 }
 
 async function download (req, res) {
