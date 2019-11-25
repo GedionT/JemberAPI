a9a42c85e7a4a815e5efb0c374d7d91e3b64660f
gediont <teshomegedion@gmail.com>
2019-11-25 15:31:53 +0300

    added authorization 4 update&changeImg on profile

--- src/profile/profileRoutes.js
+++ src/profile/profileRoutes.js
@@ -3,8 +3,8 @@ const profileController  =   require('./profileController');
 const auth               =   require('../../services/authMiddleware');
 
 
-router.post('/update',  profileController.update);
-router.post('/changeImg', profileController.changeImg);
+router.put('/update', auth, profileController.update);
+router.post('/changeImg', auth, profileController.changeImg);
 router.get('/:id', auth, profileController.getById);
 router.post('/inquire', auth, profileController.inquire);
 router.get('/', auth, profileController.getAll);
