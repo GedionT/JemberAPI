b2b59f96b872d7d32637958d60182916ab41be04
gediont <teshomegedion@gmail.com>
2019-12-01 02:51:17 +0300

    added reminder for env configuration

--- services/uploader.js
+++ services/uploader.js
@@ -7,6 +7,7 @@ module.exports = {
 }
 
 cloudinary.config({
+    //must be set using process.env or dotenv module to setup with environment
     cloud_name: 'jember',
     api_key: '244988797972126',
     api_secret: 'MxgjYxIm74MpjwVCeLByE8k6vFk'
