5270bcedde35981db88e16382cfe67fdfd4709e1
gediont <teshomegedion@gmail.com>
2019-12-01 12:18:26 +0300

    added inquiry routes and spellCheck route in file

--- src/files/fileRoutes.js
+++ src/files/fileRoutes.js
@@ -5,6 +5,8 @@ const auth               =   require('../../services/authMiddleware');
 
 router.get('/download', auth, fileController.download);
 router.post('/upload', auth, fileController.upload);
+router.put('/spellChecked', auth, fileController.spellChecked);
+router.post('/inquire', auth, fileController.inquire);
 
 
 module.exports = router;
