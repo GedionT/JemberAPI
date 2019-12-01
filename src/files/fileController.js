e6eacebe92fd1d8ede1f82d0794275fe1e375d17
gediont <teshomegedion@gmail.com>
2019-12-01 17:02:50 +0300

    added empty fileController functions

---
+++ src/files/fileController.js
@@ -0,0 +1,27 @@
+const fileDal     = require('./fileDal');
+const validator   = require('../../services/validator');
+const uploader    = require('../../services/uploader');
+
+module.exports = {
+   download, 
+   upload,
+   spellChecked,
+   inquire
+}
+
+async function download (req, res, next) {
+
+}
+
+async function upload (req, res, next) {
+
+}
+
+async function spellChecked (req, res, next) {
+
+}
+
+async function inquire (req, res, next) {
+
+
+}
