1a5c623b830c03b8195bf117e6b44e9e9e193342
gediont <teshomegedion@gmail.com>
2019-12-01 17:42:59 +0300

    minor fix

--- src/campus/campusRoutes.js
+++ src/campus/campusRoutes.js
@@ -3,7 +3,7 @@ const campusController = require('./campusController');
 const auth             = require('../../services/authMiddleware');
 
 
-router.get('/enrolledCourses/:id', auth, campusController.enrolledCourses);
+router.get('/enrolled/:id', auth, campusController.enrolledCourses);
 router.post('/addCampus', auth, campusController.addCampus);
 router.get('/getCampus', auth, campusController.getCampus);
 router.get('/getAll', auth, campusController.getAllCampuses);
