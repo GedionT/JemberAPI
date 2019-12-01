4eeb913ee4169da8108cc2c6dc7cc9e0ed9ce0f9
gediont <teshomegedion@gmail.com>
2019-12-01 12:21:28 +0300

    inquire -> addVoucher in profile routes

--- src/profile/profileRoutes.js
+++ src/profile/profileRoutes.js
@@ -6,7 +6,7 @@ const auth               =   require('../../services/authMiddleware');
 router.put('/update', auth, profileController.update);
 router.post('/changeImg', auth, profileController.changeImg);
 router.get('/:id', auth, profileController.getById);
-router.post('/inquire', auth, profileController.inquire);
+router.put('/addVoucher', auth, profileController.addVoucher);
 router.get('/', auth, profileController.getAll);
 
 module.exports = router;
