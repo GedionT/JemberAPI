ad44ef8767a45e81bc10be5c58422da30da5ca5a
gediont <teshomegedion@gmail.com>
2019-12-02 06:41:24 +0300

    added homepage dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var router        = require('express').Router();
 
+// get homepage to redirect for download
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
 router.use('/users', require('../src/users/userRoutes'));
 router.use('/profile', require('../src/profile/profileRoutes'));
 // router.use('/campus', require('../src/campus/campusRoutes'));
