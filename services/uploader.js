a0c44d9ab2855f5e4ef3c8a5a7ef81ff4aa80158
gediont <teshomegedion@gmail.com>
2019-12-10 01:14:00 +0300

    made uploader a direct cloud image uploader

--- services/uploader.js
+++ services/uploader.js
@@ -1,46 +1,19 @@
-var cloud       = require('cloudinary').v2;
-const multer    = require('multer');
+var cloudinary   = require('cloudinary').v2;
+var cloudinaryStorage = require('multer-storage-cloudinary');
+const multer     = require('multer');
 
-cloud.config({
+cloudinary.config({
     cloud_name: process.env.CLOUD_NAME || 'jember',
     api_key: process.env.CLOUD_API_KEY || '244988797972126',
     api_secret: process.env.CLOUD_API_SECRET || 'MxgjYxIm74MpjwVCeLByE8k6vFk'
 });
 
 
-const fileFilter = (req, file, cb) => {
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') 
-        cb(null, true)
-    else 
-        cb(new Error('Invalid file type, only JPEG and PNG is allowed'), false);
-}
-
-const imgUpload = multer({
-    fileFilter, 
-    storage: multer({
-        acl: 'public-read',
-        cloud,
-        bucket: 'bucket-name',
-        metadata: function(req, file, cb){
-            cb(null, {fieldName: 'testing metadata'});
-        },
-        key: function(req, file, cb) {
-            cb(null, Date.now().toString())
-        }
-    })
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "Profile Images",
+    allowedFormats: ["jpg", "png"],
+    transformation: [{width: 500, height: 500, crop: "limit"}]
 });
 
-
-function fileUpload() {
-
-}
-
-function videoUpload() {
-    
-}
-
-module.exports = {
-    imgUpload,
-    fileUpload,
-    videoUpload,
-}
+const parser = multer({storage});
