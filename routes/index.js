210208890ecebcd099aaef20de53c74a428acd1f
gediont <teshomegedion@gmail.com>
2019-12-22 10:53:27 +0300

    campus routes up and running, fixed a call from controller

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
 
 router.use('/users', require('../src/users/userRoutes'));
 router.use('/profile', require('../src/profile/profileRoutes'));
-// router.use('/campus', require('../src/campus/campusRoutes'));
+router.use('/campus', require('../src/campus/campusRoutes'));
 // router.use('/files', require('../src/files/fileRoutes'));
 
 module.exports = router;
